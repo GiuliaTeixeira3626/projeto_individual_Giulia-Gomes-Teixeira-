24bdccb6aca11d22cecf31a0439d636d6a7515ec
GiuliaTeixeira3626 <giulia.teixeira@sptech.school>
2026-05-17 19:28:26 -0300

    Adicionando novas modificações no projeto

--- Projeto/src/controllers/usuarioController.js
+++ Projeto/src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+//var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -20,20 +20,20 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
+                        //aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                            //.then((resultadoAquarios) => {
+                                //if (resultadoAquarios.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                                        //aquarios: resultadoAquarios
                                     });
-                                } else {
+                                /*} else {
                                     res.status(204).json({ aquarios: [] });
                                 }
-                            })
+                            })*/
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -54,9 +54,15 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var data = req.body.dataServer;
     var email = req.body.emailServer;
+    var telefone = req.body.telefoneServer;
+    var endereco = req.body.enderecoServer;
+    var nomeR = req.body.nomeRServer; 
+    var telefoneR = req.body.telefoneRServer;
+    var condicao = req.body.condicaoServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    //var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +71,12 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+    //} else if (fkEmpresa == undefined) {
+    //  res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, data, email, telefone, telefoneR, condicao, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
