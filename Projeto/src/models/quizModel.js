b7fcb62d6e29b5073958027f2ccbdee94fcd337b
GiuliaTeixeira3626 <giulia.teixeira@sptech.school>
2026-05-20 21:53:15 -0300

    Configurando o salvamento das respostas do quiz no banco

--- Projeto/src/models/quizModel.js
+++ Projeto/src/models/quizModel.js
@@ -1,9 +1,8 @@
 var database = require("../database/config");
 
 function salvarRespostas(id, resposta) {
-
     var instrucaoSql = `
-        INSERT INTO resposta (fk_usuario, fk_pergunta, fk_alternativa) VALUES (${resposta});
+        INSERT INTO resposta (fk_usuario, fk_pergunta, fk_alternativa) VALUES ${resposta};
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
